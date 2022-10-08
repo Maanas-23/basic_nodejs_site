7dd762b8ce16ebc65334fd931f56906f527bc161
Maanas M Singh <maanasmsingh@gmail.com>
2022-10-08 21:39:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer( (req, res) => {
+    let url = req.url.slice(1);
+    res.setHeader('Content-Type', 'text/html');
+
+    if (url === 'about') {
+        console.log(url);
+
+        fs.readFile('about.html', 'utf-8', (err, data) => {
+            res.end(data);
+        })
+
+    }
+    else {
+        console.log('no' + url);
+    }
+
+    // res.end('<h1> hello world </h1>');
+});
+
+server.listen(port, () => {
+    console.log(`Started at port ${port}`);
+});
